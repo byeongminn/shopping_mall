c3e3e594818ac558819d0e10e4379828a09c216a
byeongminn <sekn1997@naver.com>
2025-04-02 15:09:41 +0900

    refactor: SearchGoods에 GoodsGrid 컴포넌트 적용

--- src/features/search/components/SearchGoods/index.tsx
+++ src/features/search/components/SearchGoods/index.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useGetSearchGoods } from "../../hooks/useGetSearchGoods";
+import { useGetSearchGoods } from "@/features/search/hooks/useGetSearchGoods";
 import { Order } from "@/features/main/api/getGoods";
-import { VisibilityLoader } from "@/shared/components/VisibilityLoader";
-import { GoodItem } from "@/features/main/components/MainGoods/GoodItem";
-import Empty from "./empty.svg";
-import * as s from "./style.css";
+import Empty from "@/features/search/components/SearchGoods/empty.svg";
 import { formatNumberWithCommas } from "@/shared/utils/format/number";
+import { GoodsGrid } from "@/shared/components/GoodsGrid";
+import * as s from "@/features/search/components/SearchGoods/style.css";
 
 export const SearchGoods = () => {
   const searchParams = useSearchParams();
@@ -28,21 +27,18 @@ export const SearchGoods = () => {
           전체 {formatNumberWithCommas(data?.pages?.[0]?.totalResults)}
         </h6>
       </div>
-      <div className={s.dataWrapper({ isEmpty: !(flatData.length > 0) })}>
-        {flatData.length > 0 ? (
-          flatData.map((data) => <GoodItem key={data?.id} good={data} />)
-        ) : (
+      <GoodsGrid
+        goods={flatData}
+        hasNextPage={hasNextPage}
+        isFetchingNextPage={isFetchingNextPage}
+        fetchNextPage={fetchNextPage}
+        emptyContent={
           <div className={s.emptyWrapper}>
             <Empty />
             <p>앗! 찾으시는 결과가 없네요.</p>
           </div>
-        )}
-      </div>
-      {hasNextPage && !isFetchingNextPage && (
-        <VisibilityLoader
-          callback={() => !isFetchingNextPage && fetchNextPage()}
-        />
-      )}
+        }
+      />
     </>
   );
 };
--- src/features/search/components/SearchGoods/style.css.ts
+++ src/features/search/components/SearchGoods/style.css.ts
@@ -1,9 +1,12 @@
 import { style } from "@vanilla-extract/css";
-import { recipe } from "@vanilla-extract/recipes";
 import { f } from "@/shared/styles/functions";
-import { responsiveStyle } from "@/shared/styles/functions/layout.css";
 
-export const totalWrapper = style([f.wFull]);
+export const totalWrapper = style([
+  f.wFull,
+  {
+    marginBottom: "20px",
+  },
+]);
 
 export const total = style([
   {
@@ -11,40 +14,6 @@ export const total = style([
     color: "#424242",
   },
 ]);
-
-export const dataWrapper = recipe({
-  base: [
-    f.wFull,
-    {
-      marginTop: "20px",
-      display: "grid",
-    },
-  ],
-  variants: {
-    isEmpty: {
-      true: [f.flex, f.justifyCenter, f.alignCenter],
-      false: [
-        responsiveStyle({
-          lg: {
-            gridTemplateColumns: "repeat(4, 1fr)",
-            gap: "20px",
-          },
-          md: {
-            gridTemplateColumns: "repeat(3, 1fr)",
-            gap: "15px",
-          },
-          sm: {
-            gridTemplateColumns: "repeat(2, 1fr)",
-            gap: "12px 20px",
-          },
-        }),
-      ],
-    },
-  },
-  defaultVariants: {
-    isEmpty: false,
-  },
-});
 
 export const emptyWrapper = style([
   f.flex,
