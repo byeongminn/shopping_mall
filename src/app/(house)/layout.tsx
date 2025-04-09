ba4af793ca650aad92b8d93b0d28adc7a6267f32
byeongminn <sekn1997@naver.com>
2025-04-09 15:15:17 +0900

    feat: 로그인 컴포넌트 구현 및 스타일링

--- src/app/(house)/layout.tsx
+++ src/app/(house)/layout.tsx
@@ -1,14 +1,27 @@
+"use client";
+
+import { Suspense } from "react";
 import { NavigationBar } from "@/shared/components/NavigationBar";
 import { NAVIGATION_BAR_HEIGHT } from "@/shared/components/NavigationBar/style.css";
-import { Suspense } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const isNavRequired = pathname !== "/login";
+
   return (
-    <div>
-      <Suspense fallback={<></>}>
-        <NavigationBar />
-        <div style={{ paddingTop: NAVIGATION_BAR_HEIGHT + 1 }}>{children}</div>
-      </Suspense>
-    </div>
+    <Suspense fallback={<></>}>
+      {isNavRequired ? (
+        <>
+          <NavigationBar />
+          <div style={{ paddingTop: NAVIGATION_BAR_HEIGHT + 1 }}>
+            {children}
+          </div>
+        </>
+      ) : (
+        <div>{children}</div>
+      )}
+    </Suspense>
   );
 }
