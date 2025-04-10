82f746f8d511cd711464633b8f7dc426fa05a2ca
byeongminn <sekn1997@naver.com>
2025-04-10 15:27:51 +0900

    fix: 응답 인터셉터 분기 처리로 /auth/refresh API 무한 호출 버그 해결

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -22,7 +22,11 @@ api.interceptors.response.use(
       return Promise.reject(error);
     }
 
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url?.includes("/api/auth/login")
+    ) {
       originalRequest._retry = true;
 
       try {
